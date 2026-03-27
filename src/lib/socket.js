fd454e889a66c00e21562add3c00e79ce89e4137
dhavalkrish201 <dhaval.shah@krishtechnolabs.com>
2026-03-27 18:35:43 +0530

    Variable Name changes

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 let socket = null;
 
 export const connectSocket = (userId) => {
-  socket = io(import.meta.env.SOCKET_URL, {
+  socket = io(import.meta.env.VITE_SOCKET_URL, {
     query: {
       userId,
     },
