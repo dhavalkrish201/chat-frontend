d81203a6256066320866700cd1dab275fd9e8259
dhavalkrish201 <dhaval.shah@krishtechnolabs.com>
2026-03-27 16:18:42 +0530

    update env and add .env in gitignore

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -2,14 +2,11 @@ import { io } from "socket.io-client";
 let socket = null;
 
 export const connectSocket = (userId) => {
-  socket = io(
-    import.meta.env.MODE === "development" ? "http://localhost:4000" : "/",
-    {
-      query: {
-        userId,
-      },
+  socket = io(import.meta.env.SOCKET_URL, {
+    query: {
+      userId,
     },
-  );
+  });
 
   return socket;
 };
